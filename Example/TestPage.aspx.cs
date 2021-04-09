f2fdc73d1b5d0c580fb21f2a5dec25d8cd910147
nathanrobb <nathanrobb@hotmail.com>
2021-04-09 18:06:43 +1200

    Add a Test WebForm page
    - This will be used for Page constructor injection

---
+++ Example/TestPage.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.UI;
+using Example.App_Data;
+
+namespace Example
+{
+	public partial class TestPage : Page
+	{
+		private readonly TestObject _testObject;
+
+		public TestPage(TestObject testObject)
+		{
+			_testObject = testObject;
+		}
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			lbTestLabel.InnerText = _testObject.Get();
+		}
+	}
+}
