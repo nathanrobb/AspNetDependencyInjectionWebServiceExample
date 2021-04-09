e33a532f63db9683002e2a7186db74a636da0326
nathanrobb <nathanrobb@hotmail.com>
2021-04-09 18:08:36 +1200

    Add `Jehoel.AspNetDependencyInjection`

---
+++ Example/App_Start/AppStartDependencyInjection.cs
@@ -0,0 +1,39 @@
+﻿using System.Web;
+using AspNetDependencyInjection;
+using Example;
+using Example.App_Data;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(AppStartDependencyInjection), nameof(AppStartDependencyInjection.PreStart))]
+
+namespace Example
+{
+	internal class AppStartDependencyInjection
+	{
+		private static ApplicationDependencyInjection _di;
+
+		internal static void PreStart()
+		{
+			_di = new ApplicationDependencyInjectionBuilder()
+				.ConfigureServices(ConfigureServices)
+				.Build();
+		}
+
+		private static void ConfigureServices(IServiceCollection services)
+		{
+			services
+				.AddDefaultHttpContextAccessor()
+				.AddWebConfiguration();
+
+			// Custom configuration
+			services.AddScoped<TestObject>();
+		}
+
+		public static T GetService<T>()
+		{
+			return _di
+				.GetServiceProviderForCurrentHttpContext(HttpContext.Current)
+				.GetRequiredService<T>();
+		}
+	}
+}
